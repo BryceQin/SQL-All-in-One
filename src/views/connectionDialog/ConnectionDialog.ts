d2ff2a42f90baaaadc0e567134574599d7dad9b2
BryceQin <Bryce.Qin@outlook.com>
2026-06-15 09:52:14 +0800

    refactor: ConnectionDialog extends BaseWebviewPanel

--- src/views/connectionDialog/ConnectionDialog.ts
+++ src/views/connectionDialog/ConnectionDialog.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 import * as crypto from 'crypto';
-import * as fs from 'fs';
-import * as path from 'path';
+import { BaseWebviewPanel, type WebviewPanelConfig } from '../BaseWebviewPanel';
 import { getConnectionManager } from '../../database/connection/ConnectionManager';
 import { getConnectionStore } from '../../database/connection/ConnectionStore';
 import { ConnectionConfig, ConnectionGroup } from '../../database/connection/ConnectionConfig';
@@ -22,13 +21,17 @@ interface DialogMessage {
     data?: unknown;
 }
 
-export class ConnectionDialog {
-    public static currentPanel: ConnectionDialog | undefined;
+export class ConnectionDialog extends BaseWebviewPanel {
     public static readonly viewType = 'sqlAllInOneConnectionDialog';
 
-    private readonly _panel: vscode.WebviewPanel;
-    private readonly _extensionUri: vscode.Uri;
-    private _disposables: vscode.Disposable[] = [];
+    protected readonly panelConfig: WebviewPanelConfig = {
+        viewType: ConnectionDialog.viewType,
+        title: 'Connection Dialog',
+        htmlFileName: 'connection-dialog.html',
+        cssFileName: 'connection-dialog.css',
+        jsFileName: 'connection-dialog.js',
+    };
+
     private _mode: 'create' | 'edit' = 'create';
     private _connectionId?: string;
     private _treeProvider?: DatabaseTreeProvider;
@@ -42,32 +45,27 @@ export class ConnectionDialog {
             treeProvider?: DatabaseTreeProvider;
         }
     ): Promise<{ saved: boolean; connectionId?: string } | undefined> {
-        if (ConnectionDialog.currentPanel) {
-            ConnectionDialog.currentPanel._panel.reveal();
+        const existing = BaseWebviewPanel.getExistingInstance<ConnectionDialog>(ConnectionDialog.viewType);
+        if (existing) {
+            BaseWebviewPanel.revealExisting(ConnectionDialog.viewType);
             return undefined;
         }
 
-        const panel = vscode.window.createWebviewPanel(
+        const panel = BaseWebviewPanel.createWebviewPanel(
             ConnectionDialog.viewType,
             options.mode === 'create' ? t('connDialog.newConnection') : t('connDialog.editConnection'),
-            vscode.ViewColumn.One,
-            {
-                enableScripts: true,
-                localResourceRoots: [
-                    vscode.Uri.joinPath(extensionUri, 'media'),
-                ],
-                retainContextWhenHidden: true,
-            }
+            extensionUri,
+            { viewColumn: vscode.ViewColumn.One }
         );
 
         const dialog = new ConnectionDialog(panel, extensionUri, options.treeProvider);
-        ConnectionDialog.currentPanel = dialog;
+        BaseWebviewPanel.registerInstance(dialog);
 
         dialog._mode = options.mode;
         dialog._connectionId = options.connectionId;
 
         const config = await dialog._buildConfig(options);
-        await dialog._update(config);
+        await dialog._initializeWithConfig(config);
 
         return new Promise<{ saved: boolean; connectionId?: string } | undefined>((resolve) => {
             dialog._resolveDialog = resolve;
@@ -79,32 +77,96 @@ export class ConnectionDialog {
         extensionUri: vscode.Uri,
         treeProvider?: DatabaseTreeProvider
     ) {
-        this._panel = panel;
-        this._extensionUri = extensionUri;
+        super(panel, extensionUri);
         this._treeProvider = treeProvider;
+    }
 
-        this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+    private async _initializeWithConfig(config: ConnectionDialogConfig): Promise<void> {
+        const configScript = '<script>window.__CONNECTION_DIALOG_CONFIG__ = ' + JSON.stringify(config) + ';</script>';
 
-        this._panel.webview.onDidReceiveMessage(
-            async (message: DialogMessage) => {
-                switch (message.command) {
-                    case 'save':
-                        await this._handleSave(message.data as ConnectionConfig);
-                        break;
-                    case 'testConnection':
-                        await this._handleTestConnection(message.data as ConnectionConfig);
-                        break;
-                    case 'close':
-                        this._closeDialog({ saved: false });
-                        break;
-                    case 'browseFile':
-                        await this._handleBrowseFile(message.data as { field: string });
-                        break;
-                }
-            },
-            null,
-            this._disposables
-        );
+        const i18nData: Record<string, string> = {
+            'newConnection': t('connDialog.newConnection'),
+            'editConnection': t('connDialog.editConnection'),
+            'configureDatabaseConnection': t('connDialog.configureParams'),
+            'databaseType': t('connDialog.databaseType'),
+            'connectionName': t('connDialog.connectionName'),
+            'connectionNamePlaceholder': t('connDialog.connectionNamePh'),
+            'group': t('connDialog.group'),
+            'noGroup': t('connDialog.noGroup'),
+            'colorTag': t('connDialog.colorTag'),
+            'host': t('connDialog.host'),
+            'port': t('connDialog.port'),
+            'username': t('connDialog.username'),
+            'password': t('connDialog.password'),
+            'enterPassword': t('connDialog.enterPassword'),
+            'database': t('connDialog.database'),
+            'databaseFilePath': t('connDialog.databaseFilePath'),
+            'databaseFilePathPlaceholder': t('connDialog.databaseFilePathPh'),
+            'browse': t('connDialog.browse'),
+            'useSshTunnel': t('connDialog.useSshTunnel'),
+            'sshHost': t('connDialog.sshHost'),
+            'sshPort': t('connDialog.sshPort'),
+            'authenticationMethod': t('connDialog.authenticationMethod'),
+            'privateKey': t('connDialog.authPrivateKey'),
+            'sshPassword': t('connDialog.sshPasswordPh'),
+            'passphrase': t('connDialog.passphrase'),
+            'optionalPassphrase': t('connDialog.passphrasePh'),
+            'useSsl': t('connDialog.useSsl'),
+            'caCertificate': t('connDialog.caCertificate'),
+            'clientCertificate': t('connDialog.clientCertificate'),
+            'clientKey': t('connDialog.clientKey'),
+            'verifyServerCertificate': t('connDialog.verifyServerCert'),
+            'connectTimeout': t('connDialog.connectTimeout'),
+            'poolSize': t('connDialog.poolSize'),
+            'charset': t('connDialog.charset'),
+            'timezone': t('connDialog.timezone'),
+            'initialSql': t('connDialog.initialSql'),
+            'test': t('connDialog.test'),
+            'cancel': t('connDialog.cancel'),
+            'save': t('connDialog.save'),
+            'testing': t('connDialog.testing'),
+            'connectionSuccessful': t('connDialog.connectionSuccessful'),
+            'connectionFailed': t('connDialog.connectionFailed'),
+            'saveFailed': t('connDialog.saveFailed'),
+            'unknownError': t('connDialog.unknownError'),
+            'nameRequired': t('connDialog.nameRequired'),
+            'nameExists': t('connDialog.nameExists'),
+            'hostRequired': t('connDialog.hostRequired'),
+            'portRange': t('connDialog.portRange'),
+            'usernameRequired': t('connDialog.usernameRequired'),
+            'sqlitePathRequired': t('connDialog.sqlitePathRequired'),
+            'sshHostRequired': t('connDialog.sshHostRequired'),
+            'sshPortRange': t('connDialog.sshPortRange'),
+            'sshUsernameRequired': t('connDialog.sshUsernameRequired'),
+            'newGroup': t('connDialog.newGroup'),
+            'enterNewGroupName': t('connDialog.enterNewGroupName'),
+            'selectFile': t('connDialog.selectFile'),
+            'none': t('connDialog.none'),
+        };
+        const i18nScript = '<script>window.__CONNECTION_DIALOG_I18N__ = ' + JSON.stringify(i18nData) + ';</script>';
+
+        await this.initializeHtml([
+            { placeholder: '{{CONFIG_INJECT}}', value: configScript },
+            { placeholder: '{{I18N_INJECT}}', value: i18nScript },
+        ]);
+
+        this.onDidReceiveMessage(async (message: unknown) => {
+            const msg = message as DialogMessage;
+            switch (msg.command) {
+                case 'save':
+                    await this._handleSave(msg.data as ConnectionConfig);
+                    break;
+                case 'testConnection':
+                    await this._handleTestConnection(msg.data as ConnectionConfig);
+                    break;
+                case 'close':
+                    this._closeDialog({ saved: false });
+                    break;
+                case 'browseFile':
+                    await this._handleBrowseFile(msg.data as { field: string });
+                    break;
+            }
+        });
     }
 
     private async _buildConfig(options: { mode: 'create' | 'edit'; connectionId?: string }): Promise<ConnectionDialogConfig> {
@@ -136,112 +198,10 @@ export class ConnectionDialog {
         };
     }
 
-    private async _update(config: ConnectionDialogConfig): Promise<void> {
-        const html = await this._getHtmlForWebview(config);
-        this._panel.webview.html = html;
-    }
-
-    private async _getHtmlForWebview(config: ConnectionDialogConfig): Promise<string> {
-        try {
-            const htmlPath = path.join(
-                this._extensionUri.fsPath,
-                'media',
-                'connection-dialog.html'
-            );
-            let html = await fs.promises.readFile(htmlPath, 'utf-8');
-
-            const cssUri = this._panel.webview.asWebviewUri(
-                vscode.Uri.joinPath(this._extensionUri, 'media', 'connection-dialog.css')
-            );
-            const jsUri = this._panel.webview.asWebviewUri(
-                vscode.Uri.joinPath(this._extensionUri, 'media', 'connection-dialog.js')
-            );
-
-            html = html.replace('{{CSS_URI}}', cssUri.toString());
-            html = html.replace('{{JS_URI}}', jsUri.toString());
-            html = html.replace(/\{\{CSP_SOURCE\}\}/g, this._panel.webview.cspSource);
-
-            const nonce = crypto.randomUUID();
-            html = html.replace(/\{\{CSP_NONCE\}\}/g, nonce);
-            html = html.replace(/<script(?=\s)/g, `<script nonce="${nonce}"`);
-            html = html.replace(/<style(?=\s)/g, `<style nonce="${nonce}"`);
-
-            const configScript = '<script nonce="' + nonce + '">window.__CONNECTION_DIALOG_CONFIG__ = ' + JSON.stringify(config) + ';</script>';
-            html = html.replace('{{CONFIG_INJECT}}', configScript);
-
-            const i18nData: Record<string, string> = {
-                'newConnection': t('connDialog.newConnection'),
-                'editConnection': t('connDialog.editConnection'),
-                'configureDatabaseConnection': t('connDialog.configureParams'),
-                'databaseType': t('connDialog.databaseType'),
-                'connectionName': t('connDialog.connectionName'),
-                'connectionNamePlaceholder': t('connDialog.connectionNamePh'),
-                'group': t('connDialog.group'),
-                'noGroup': t('connDialog.noGroup'),
-                'colorTag': t('connDialog.colorTag'),
-                'host': t('connDialog.host'),
-                'port': t('connDialog.port'),
-                'username': t('connDialog.username'),
-                'password': t('connDialog.password'),
-                'enterPassword': t('connDialog.enterPassword'),
-                'database': t('connDialog.database'),
-                'databaseFilePath': t('connDialog.databaseFilePath'),
-                'databaseFilePathPlaceholder': t('connDialog.databaseFilePathPh'),
-                'browse': t('connDialog.browse'),
-                'useSshTunnel': t('connDialog.useSshTunnel'),
-                'sshHost': t('connDialog.sshHost'),
-                'sshPort': t('connDialog.sshPort'),
-                'authenticationMethod': t('connDialog.authenticationMethod'),
-                'privateKey': t('connDialog.authPrivateKey'),
-                'sshPassword': t('connDialog.sshPasswordPh'),
-                'passphrase': t('connDialog.passphrase'),
-                'optionalPassphrase': t('connDialog.passphrasePh'),
-                'useSsl': t('connDialog.useSsl'),
-                'caCertificate': t('connDialog.caCertificate'),
-                'clientCertificate': t('connDialog.clientCertificate'),
-                'clientKey': t('connDialog.clientKey'),
-                'verifyServerCertificate': t('connDialog.verifyServerCert'),
-                'connectTimeout': t('connDialog.connectTimeout'),
-                'poolSize': t('connDialog.poolSize'),
-                'charset': t('connDialog.charset'),
-                'timezone': t('connDialog.timezone'),
-                'initialSql': t('connDialog.initialSql'),
-                'test': t('connDialog.test'),
-                'cancel': t('connDialog.cancel'),
-                'save': t('connDialog.save'),
-                'testing': t('connDialog.testing'),
-                'connectionSuccessful': t('connDialog.connectionSuccessful'),
-                'connectionFailed': t('connDialog.connectionFailed'),
-                'saveFailed': t('connDialog.saveFailed'),
-                'unknownError': t('connDialog.unknownError'),
-                'nameRequired': t('connDialog.nameRequired'),
-                'nameExists': t('connDialog.nameExists'),
-                'hostRequired': t('connDialog.hostRequired'),
-                'portRange': t('connDialog.portRange'),
-                'usernameRequired': t('connDialog.usernameRequired'),
-                'sqlitePathRequired': t('connDialog.sqlitePathRequired'),
-                'sshHostRequired': t('connDialog.sshHostRequired'),
-                'sshPortRange': t('connDialog.sshPortRange'),
-                'sshUsernameRequired': t('connDialog.sshUsernameRequired'),
-                'newGroup': t('connDialog.newGroup'),
-                'enterNewGroupName': t('connDialog.enterNewGroupName'),
-                'selectFile': t('connDialog.selectFile'),
-                'none': t('connDialog.none'),
-            };
-            const i18nScript = '<script nonce="' + nonce + '">window.__CONNECTION_DIALOG_I18N__ = ' + JSON.stringify(i18nData) + ';</script>';
-            html = html.replace('{{I18N_INJECT}}', i18nScript);
-
-            return html;
-        } catch (error) {
-            console.error('Failed to load Connection Dialog HTML:', error);
-            return '<html><body><h2>' + t('connDialog.loadFailed') + '</h2><p>' + t('connDialog.reinstall') + '</p></body></html>';
-        }
-    }
-
     private async _handleSave(formData: ConnectionConfig): Promise<void> {
         const validationError = this._validateForm(formData);
         if (validationError) {
-            this._panel.webview.postMessage({
+            this.postMessage({
                 command: 'saveResult',
                 success: false,
                 error: validationError,
@@ -279,7 +239,7 @@ export class ConnectionDialog {
             this._treeProvider?.refresh();
             this._closeDialog({ saved: true, connectionId: this._connectionId });
         } catch (error) {
-            this._panel.webview.postMessage({
+            this.postMessage({
                 command: 'saveResult',
                 success: false,
                 error: (error as Error).message,
@@ -292,7 +252,7 @@ export class ConnectionDialog {
         const store = getConnectionStore();
 
         try {
-            this._panel.webview.postMessage({ command: 'testStart' });
+            this.postMessage({ command: 'testStart' });
 
             let password = formData.password;
             let sshPassword = formData.ssh?.password;
@@ -317,7 +277,7 @@ export class ConnectionDialog {
 
             const result = await manager.testConnection(testConfig, password);
 
-            this._panel.webview.postMessage({
+            this.postMessage({
                 command: 'testResult',
                 success: result.success,
                 serverVersion: result.serverVersion,
@@ -325,7 +285,7 @@ export class ConnectionDialog {
                 error: result.error,
             });
         } catch (error) {
-            this._panel.webview.postMessage({
+            this.postMessage({
                 command: 'testResult',
                 success: false,
                 error: (error as Error).message,
@@ -342,7 +302,7 @@ export class ConnectionDialog {
         });
 
         if (uris && uris.length > 0) {
-            this._panel.webview.postMessage({
+            this.postMessage({
                 command: 'fileSelected',
                 field: data.field,
                 path: uris[0].fsPath,
@@ -392,19 +352,11 @@ export class ConnectionDialog {
         this.dispose();
     }
 
-    public dispose(): void {
-        ConnectionDialog.currentPanel = undefined;
+    public override dispose(): void {
         if (this._resolveDialog) {
             this._resolveDialog(undefined);
             this._resolveDialog = undefined;
         }
-        this._panel.dispose();
-
-        while (this._disposables.length) {
-            const x = this._disposables.pop();
-            if (x) {
-                x.dispose();
-            }
-        }
+        super.dispose();
     }
 }
